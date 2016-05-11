5ed8e89188bee655218a835f6370404426f41851
James Hornung <jameshornung@Jamess-MacBook-Pro.local>
2016-05-11 18:01:13 -0500

    resolved issue with being allowed to guess duplicate letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,25 @@
-//global variable
-var wordBank = ["dog", "bird", "pidgeon"];
+//variable
+var wordBank = ["zilker", "barton", "butler", "horns", "sixth"];
 var targetWord = "";
 var targetWordArray = [];
 var wrongLetters = [];
 var correctLetters = [];
 var displayWord = [];
+var valid;
 var wins = 0;
 var losses = 0;
 var remainingGuesses = 10;
-
-
+var letterGuessed;
 
 //functions
+
+//selects a word from word bank and displays all info on gamescreen
 function startGame()
 {
+	//selects random word
 	targetWord = wordBank[Math.floor(Math.random()* wordBank.length)];
 	targetWordArray = targetWord.split("");
-
+	//transforms word to an array of dashes
 	for(i=0; i<targetWord.length; i++)
 	{
 		displayWord.push(" _ ");
@@ -30,6 +33,7 @@ function startGame()
 	document.getElementById("letters-guessed-box").innerHTML = "Letters Guessed: " + wrongLetters;
 }
 
+//checks to see if letter entered by user is in the target word
 function checkLetter(letter)
 {
 	var isInWord;
@@ -52,12 +56,12 @@ function checkLetter(letter)
 		remainingGuesses = remainingGuesses - 1;
 	}
 	else
-	{
-		
+	{	
 	}
 	document.getElementById("array-box").innerHTML = displayWord.join("");
 }
 
+//resets the variables to original values
 function reset()
 {
 	targetWord = "";
@@ -69,18 +73,18 @@ function reset()
 
 }
 
+//replaces  "-" with character on the screen
 function replaceLetter(letter)
 {
 	var j = targetWordArray.indexOf(letter);
 	displayWord.splice(j, 1, letter);
 }
 
+//evaluates if the game has been won
 function checkForWin()
 {
-
 	if(correctLetters.length == targetWordArray.length)
 	{
-		document.getElementById("remaining-guesses").innerHTML = "YOU WIN, YOU WIN, YOU WIN";
 		wins = wins + 1;
 		document.getElementById("wins").innerHTML = "Wins: " + wins;
 		reset();
@@ -88,6 +92,20 @@ function checkForWin()
 	}
 }
 
+//allows only characters AND only characters that have not yet been guessed
+function validateKey(keystroke)
+{
+	if (correctLetters.includes(letterGuessed) || wrongLetters.includes(letterGuessed))
+	{
+		valid = false;
+	}
+	else
+	{
+		valid = true;
+	}
+}
+
+//plays music
 function playMusic(){
 	document.getElementById("moon-song").play();
 }
@@ -102,13 +120,13 @@ document.onkeyup = function(event)
 {
 	console.log(targetWordArray);
 	console.log(displayWord);
-	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+	letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+	validateKey();
+	if (valid == true)
+	{
 	checkLetter(letterGuessed);
 	document.getElementById("remaining-guesses").innerHTML = "Guesses Remaing: " + remainingGuesses;
 	document.getElementById("letters-guessed-box").innerHTML = "Letters Guessed: " + wrongLetters;
-
-	console.log(correctLetters.length);
-	console.log(targetWordArray.length);
 
 	checkForWin();
 
@@ -119,7 +137,7 @@ document.onkeyup = function(event)
 		losses = losses + 1;
 		document.getElementById("losses").innerHTML = "Losses: " + losses;
 	}
-
+	}
 }
 console.log(displayWord);
 //troubleshoot
